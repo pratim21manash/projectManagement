60891fbc4c35e43fb61a1cadfc7501ad7b244ae0
manash pratim kashyap <pratimkashyapm@gmail.com>
2026-03-25 22:48:25 +0530

    cors and global api success class setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
 
@@ -6,6 +7,13 @@ const app = express()
 app.use(express.json({limit: "16kb"})) // to make readable clients json.body
 app.use(express.urlencoded({extended: true, limit: "16kb"})) // this will encode our url for safety reson
 app.use(express.static('public')) // this tells express about never changing files/data
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+    methods: ['GET', "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+})) 
 
 app.get("/", (req,res)=>{
     res.end("Hello World India")
