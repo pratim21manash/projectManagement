e4dc7c840292f4893dacc04defb7780a34509ea8
manash pratim kashyap <pratimkashyapm@gmail.com>
2026-03-25 22:01:13 +0530

    Server setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config({
     path: './.env',
 })
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log("Server is running"))
