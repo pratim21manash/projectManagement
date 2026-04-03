5ddb63ac9ed930f352b208b952d9a22744d000c1
manash pratim kashyap <pratimkashyapm@gmail.com>
2026-04-03 09:03:49 +0530

    token setup

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,59 +1,113 @@
-import { Schema, model } from "mongoose"
-import bcrypt from "bcrypt"
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     avatar: {
-        type: {
-            url: String,
-        },
-        default: {
-            url: "https://placehold.co/200x200",
-        }
+      type: {
+        url: String,
+      },
+      default: {
+        url: "https://placehold.co/200x200",
+      },
     },
     username: {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
-    fullname: {
-        type: String,
-        trim: true,
+    fullName: {
+      type: String,
+      trim: true,
     },
     password: {
-        type: String,
-        required: [true, "Password is required"]
+      type: String,
+      required: [true, "Password is required"],
     },
     isEailVerified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     refreshToken: {
-        type: String
+      type: String,
     },
     forgotPasswordToken: {
-        type: String
+      type: String,
     },
     forgotPasswordTokenExpiry: {
-        type: Date
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: Date,
     },
     emailVerificationTokenExpiry: {
-        type: Date
-    }
-}, { timestamps: true })
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
 
 //Hooks
-userSchema.pre('save', async function(next){
-    await bcrypt.hash(this.password, 10)
-    next()
-})
-   
-const UserModel = model("User", userSchema)
-export {UserModel}
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    return next();
+  } else {
+    await bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+
+//Methods
+//comapre password
+userSchema.methods.isPasswordCorret = async function (password) {
+  await bcrypt.compare(password, this.password);
+};
+
+//Generate access Token
+userSchema.methods.generateAccessToken = function () {
+  const payload = {
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+  };
+
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRY,
+  });
+};
+
+//Generate jwt refresh Token
+userSchema.methods.generateRefreshToken = function () {
+  const payload = {
+    _id: this._id,
+  };
+
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+//Temporary tokens for email verification and password reset
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 20 * 60 * 1000;
+};
+
+const UserModel = model("User", userSchema);
+export { UserModel };
